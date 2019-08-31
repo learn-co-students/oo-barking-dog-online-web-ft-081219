10b4f9e78641b395754b8cdf88bd12ee2989c18e
Emilia ☆ <emilia.mena26@gmail.com>
2019-08-31 22:14:40 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,24 @@
 class Dog 
-  attr_accessor :name
- def bark
-   puts "woof!"
+
+attr_accessor :name 
+
+  def bark 
+    puts "woof!"
   end
-end 
+
+end
+
+# class Dog 
+  
+#   def name=(name)
+#     @name=name
+#   end
+  
+#   def name
+#     @name 
+#   end
+  
+#   def bark
+#     puts "woof!"
+#   end
+# end 
